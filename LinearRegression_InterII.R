07c7aa066ed2a5134cbadb4a5157b9d4e7acafdf
wonter123 <lurunjie@hotmail.com>
2017-11-14 11:38:50 -0500

    Update LinearRegression_InterII.R

--- LinearRegression_InterII.R
+++ LinearRegression_InterII.R
@@ -1,6 +1,11 @@
 pData = read.csv("PilgrimCaseData.csv")
 
-model9 <- lm(X9Profit~factor(X9Online)+factor(X9Age)+X9Inc+X9Tenure+factor(X9District)+factor(X9Billpay),data = pData)
+modelOnline9 <- lm(pData$X9Profit~factor(pData$X9Online))
+summary(modelOnline9)
+
+plot(modelOnline9,main = "Linear Regression between profitabiliy and Online of 1999")
+
+model9 <- lm(X9Profit~factor(X9Online)+X9Age+X9Inc+X9Tenure+factor(X9District)+factor(X9Billpay),data = pData)
 summary(model9)
 
 plot(model9, main = "Linear Regression of 1999")
