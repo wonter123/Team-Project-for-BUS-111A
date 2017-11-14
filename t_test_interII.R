1d894fc0440018a742b05bc03ef327f5c4d54d65
wonderfulife <lurunjie@hotmail.com>
2017-11-14 09:00:52 -0500

    update t test

--- t_test_interII.R
+++ t_test_interII.R
@@ -7,3 +7,9 @@ pData_withOnline0 = pData[pData$X0Online==1,]
 pData_withoutOnline0 = pData[pData$X0Online==0,]
 t.test(pData_withOnline0$X0Profit,pData_withoutOnline0$X0Profit,paired = FALSE)
 
+pData_withEbilling9 = pData[pData$X9Billpay == 1,]
+pData_withoutEbilling9 = pData[pData$X9Billpay == 0,]
+t.test(pData_withEbilling9$X9Profit,pData_withoutEbilling9$X9Profit,paired = FALSE)
+pData_withEbilling0 = pData[pData$X0Billpay == 1,]
+pData_withoutEbilling0 = pData[pData$X0Billpay == 0,]
+t.test(pData_withEbilling0$X0Profit,pData_withoutEbilling0$X0Profit,paired = FALSE)
