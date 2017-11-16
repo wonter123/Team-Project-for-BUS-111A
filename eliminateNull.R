8f4622544b933a348344b422373ca9f17c4097a6
wonderfulife <lurunjie@hotmail.com>
2017-11-15 20:57:38 -0500

    update eliminate null

--- eliminateNull.R
+++ eliminateNull.R
@@ -1,5 +1,5 @@
 eliminateNull <- function(x) { 
-  meanX = mean(x[!is.na(x)])
+  meanX = mean(x,na.rm = TRUE)
   for (i in 1:length(x)){
     if (is.na(x[i])) {
       #print(i)
@@ -9,10 +9,29 @@ eliminateNull <- function(x) {
   return(x)
 }
 
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+eliminateNullFactor <- function(x) { 
+  modeX = getmode(x[!is.na(x)])
+  print(modeX)
+  for (i in 1:length(x)){
+    if (is.na(x[i])) {
+      #print(i)
+      x[i] = modeX
+    }
+  }
+  return(x)
+}
+
 pcData = read.csv("PilgrimCaseData.csv")
 
+
 pData$X9Profit = eliminateNull(pcData$X9Profit)
-pData$X9Age = eliminateNull(pcData$X9Age)
-pData$X9Inc = eliminateNull(pcData$X9Inc)
+pData$X9Age = eliminateNullFactor(pcData$X9Age)
+pData$X9Inc = eliminateNullFactor(pcData$X9Inc)
 pData$X9Tenure = eliminateNull(pcData$X9Tenure)
 pData$X0Profit = eliminateNull(pcData$X0Profit)
+pData$X9District = eliminateNullFactor(pcData$X9District)
