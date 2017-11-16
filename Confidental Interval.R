5b5e1f3fe81601bcfea71e6d3b0fd0f12ef50074
wonderfulife <lurunjie@hotmail.com>
2017-11-15 21:35:40 -0500

    new confidental interval

--- Confidental Interval.R
+++ Confidental Interval.R
@@ -1,14 +1,16 @@
 pcData = read.csv("PilgrimCaseData.csv")
-####pcData[!complete.cases(pcData),]
-#####listAge = pcData$X9Age
-#####listAge <- listAge[!is.na(listAge)]viewmean
+
 listProfit = pcData$X9Profit
 mean(listProfit)
+
+#Create a new matrix with age, income and profit
 listAge_Profit <- matrix(c(pcData$X9Age,pcData$X9Inc,pcData$X9Profit), ncol = 3)
+
+#Create another matrix without the missing data(eliminate those rows which have missing data)
 listAge_Profit_NoNA = listAge_Profit[!is.na(listAge_Profit[,1]),]
 listAge_Profit_NoNA = listAge_Profit_NoNA[!is.na(listAge_Profit_NoNA[,2]),]
 listAge_Profit_NoNA = listAge_Profit_NoNA[!is.na(listAge_Profit_NoNA[,1]),]
-###temp = listAge_Profit_NoNA[,2]
+
 View(listAge_Profit_NoNA)
 length(listAge_Profit_NoNA)
 mean_WNA = mean(listAge_Profit[,3])
@@ -17,7 +19,9 @@ mean_NoNA = mean(listAge_Profit_NoNA[,3])
 sd_NONA = sd(listAge_Profit_NoNA[,3])
 size_NoNA = length(listAge_Profit_NoNA) / 3
 var_NoNA = var(listAge_Profit_NoNA[,3])
-####error_NoNA <- qt(0.975,df=size_NoNA-1)*sd_NONA/sqrt(size_NoNA)
 
-#### t = (mean_NoNA - mean_WNA) / (sd_NONA/sqrt(size_NoNA))
+#t test whether missing data effect the result
 t.test(listAge_Profit[,3],listAge_Profit_NoNA[,3],paired = FALSE)
+
+#t test whether handling missing data with mode affect the result
+t.test(listAge_Profit[,3],pData$X9Profit,paired = FALSE)
