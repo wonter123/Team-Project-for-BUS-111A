4263966c55924c510ff20a17dc382b880f72928a
wonderfulife <lurunjie@hotmail.com>
2017-11-27 21:49:58 -0500

    gaoding

--- RetentionRegression.R
+++ RetentionRegression.R
@@ -1,5 +1,5 @@
-##modelOnline9 <- lm(pData$X9Profit~factor(pData$X9Online))
-##summary(modelOnline9)
+modelOnline9 <- lm(pData$X9Profit~factor(pData$X9Online))
+summary(modelOnline9)
 
 
 modelRetention <- lm(pData$Retention~pData$X9Online)
