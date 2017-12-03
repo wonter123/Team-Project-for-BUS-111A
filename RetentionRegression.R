e0d7733f3d773adc43c329215863dd6ae140f3c2
wonderfulife <lurunjie@hotmail.com>
2017-12-02 21:09:25 -0500

    ha

--- RetentionRegression.R
+++ RetentionRegression.R
@@ -8,3 +8,9 @@ summary(modelRetention)
 modelRetention2 <- lm(pData$Retention~pData$X9Online+pData$X9Billpay)
 summary(modelRetention2)
 
+modelRetention3 <- lm(pData$X9Profit~pData$X9Online+pData$X9Billpay)
+summary(modelRetention3)
+
+modelRetention4 <- lm(pData$X0Profit~pData$X0Online+pData$X0Billpay)
+summary(modelRetention4)
+
