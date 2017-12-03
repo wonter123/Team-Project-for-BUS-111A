51db091dd754da83b6f15efd3024301464c56feb
wonderfulife <lurunjie@hotmail.com>
2017-12-02 20:38:59 -0500

    retention update

--- RetentionRegression.R
+++ RetentionRegression.R
@@ -5,3 +5,6 @@ summary(modelOnline9)
 modelRetention <- lm(pData$Retention~pData$X9Online)
 summary(modelRetention)
 
+modelRetention2 <- lm(pData$Retention~pData$X9Online+pData$X9Billpay)
+summary(modelRetention2)
+
